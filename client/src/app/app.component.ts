7485d97eb9c382e086b58d7fa1a4f87c2a5c7034
vic <victordjohnson@gmail.com>
2021-06-18 12:32:18 -0500

    Add account service for login/logout functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { AccountService } from './_services/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +12,30 @@ export class AppComponent {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
     this.getUsers();
+    this.setCurrentUser();
   }
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(
       (response) => {
         this.users = response;
-        console.log(this.users)
+        console.log(this.users);
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+
+  setCurrentUser() {
+    const userData = localStorage.getItem('user') ?? '';
+    const user: User = JSON.parse(userData);
+    this.accountService.setCurrentUser(user);
   }
 }
