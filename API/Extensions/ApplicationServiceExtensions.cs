7f5cd531fc25f21cf5f0c8ac112e6701518b4b08
Victor Johnson <VictorDJohnson@gmail.com>
2022-09-09 19:42:12 -0500

    connection string

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -55,7 +55,7 @@ namespace API.Extensions
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSLMode=Require;TrustServerCertificate=True";
                     Console.WriteLine($"Connection String: ${connStr}");
                 }
 
